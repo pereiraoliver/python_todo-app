3b83ad87bf6b4682acf0082aba35535ec790d0ce
Oliver Pereira <pereira.oliver@gmail.com>
2026-08-15 17:24:57 +0530

    Complete PDF Generator App

--- App2_PDF_Generator/main.py
+++ App2_PDF_Generator/main.py
@@ -2,14 +2,34 @@ import pandas as pd
 from fpdf import FPDF
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
+
+
+def setFooter():
+    pdf.set_draw_color(180, 180, 180)
+    pdf.line(10, 285, 200, 285)
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
 
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=25)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.set_draw_color(100, 100, 100)
     pdf.line(10, 21, 200, 21)
+    pdf.ln(265)
+    # Set the footer
+    setFooter()
+
+    for p in range(row["Pages"] - 1):
+        pdf.add_page()
+        pdf.ln(277)
+        setFooter()
+
 
 pdf.output("output.pdf")
