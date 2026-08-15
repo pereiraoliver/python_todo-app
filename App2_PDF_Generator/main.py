8c116188b3048a990a451f39f212c7dfc88de94c
Oliver Pereira <pereira.oliver@gmail.com>
2026-08-15 17:36:29 +0530

    Completed Student Project

--- App2_PDF_Generator/main.py
+++ App2_PDF_Generator/main.py
@@ -8,8 +8,6 @@ df = pd.read_csv("topics.csv")
 
 
 def setFooter():
-    pdf.set_draw_color(180, 180, 180)
-    pdf.line(10, 285, 200, 285)
     pdf.set_font(family="Times", style="I", size=10)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
@@ -25,11 +23,15 @@ for index, row in df.iterrows():
     pdf.ln(265)
     # Set the footer
     setFooter()
+    for l in range(25):
+        pdf.line(10, 21 + (l * 12), 200, 21 + (l * 12))
 
     for p in range(row["Pages"] - 1):
         pdf.add_page()
         pdf.ln(277)
         setFooter()
+        for l in range(25):
+            pdf.line(10, 21 + (l * 12), 200, 21 + (l * 12))
 
 
 pdf.output("output.pdf")
